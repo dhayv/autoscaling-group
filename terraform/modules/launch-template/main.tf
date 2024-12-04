aeae8c81069c2268b0f2c7ef373b838451b89f80
Hayvid <davidhyppolitedh@gmail.com>
2024-12-03 19:38:29 -0500

    update template to now include iam role for ec2 and alb

--- terraform/modules/launch-template/main.tf
+++ terraform/modules/launch-template/main.tf
@@ -102,5 +102,64 @@ resource "aws_launch_template" "main" {
     enabled = "true"
   }
 
-  user_data = filebase64("${path.root.scripts}/ec2.sh")
+  user_data = filebase64("${abspath(path.root)}/scripts/ec2.sh")
+
+  iam_instance_profile {
+    name = aws_iam_instance_profile.ec2_profile.name
+  }
 }
+
+resource "aws_iam_role" "ec2_role" {
+  name = "${var.project_name}-ec2-role"
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+resource "aws_iam_instance_profile" "ec2_profile" {
+  name = "${var.project_name}-ec2-profile"
+  role = aws_iam_role.ec2_role.name
+}
+
+
+resource "aws_iam_role_policy" "ec2_custom_policy" {
+  name = "${var.project_name}-ec2-custom-policy"
+  role = aws_iam_role.ec2_role.name
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "elasticloadbalancing:Describe*",
+          "elasticloadbalancing:DeregisterTargets",
+          "elasticloadbalancing:RegisterTargetsyes"
+        ]
+        Resource = "*"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "ec2:DescribeInstances",
+          "ec2:DescribeTags"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
